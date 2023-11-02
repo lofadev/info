2958b6349cab3b2aeb9e99404010f21a449ea4fe
LofA <contact.lofa@gmail.com>
2023-11-02 11:42:42 +0700

    update info_link and calulator my age

--- main.js
+++ main.js
@@ -6,22 +6,27 @@ window.onload = function () {
   const menu = document.querySelector('.menu');
   const menuBar = document.querySelector('.menu__bar');
   const modal = document.querySelector('.modal');
+  const ageElement = document.querySelector('.info-age');
 
-  let currentIndex = 0;
-  let translateValue;
-  let isCheck = false;
+  let currentIndex = 0,
+    translateValue,
+    isCheck = false;
+
+  // handle event logo home
   logo.onclick = () => {
     wrapper.style = `transform: translateX(0px)`;
     document.querySelector('.menu__items.active').classList.remove('active');
     menuItems[0].classList.add('active');
   };
 
+  // hande resize on window
   window.onresize = () => {
     contentWidth = wrapper.clientWidth;
     translateValue = contentWidth * currentIndex + currentIndex * 50;
     wrapper.style = `transform: translateX(-${translateValue}px)`;
   };
 
+  // handle menu item click
   menuItems.forEach((item, index) => {
     item.addEventListener('click', () => {
       currentIndex = index;
@@ -36,6 +41,7 @@ window.onload = function () {
     });
   });
 
+  // handle menu bar on mobile
   menuBar.onclick = () => {
     isCheck = !isCheck;
     if (isCheck) {
@@ -49,10 +55,29 @@ window.onload = function () {
     }
   };
 
+  // handle modal background on mobile
   modal.onclick = () => {
     isCheck = !isCheck;
     menu.style = 'transform: scale(0)';
     modal.style = 'display: none';
     menuBar.classList.remove('active');
   };
+
+  // Ngày sinh
+  const birthday = new Date('2002-08-08');
+  // Ngày hiện tại
+  const today = new Date();
+
+  // Tính toán tuổi
+  const age = today.getFullYear() - birthday.getFullYear();
+
+  // Kiểm tra xem đã qua sinh nhật chưa trong năm nay
+  if (
+    today.getMonth() < birthday.getMonth() ||
+    (today.getMonth() === birthday.getMonth() && today.getDate() < birthday.getDate())
+  ) {
+    age--; // Chưa qua sinh nhật năm nay
+  }
+
+  ageElement.innerText = age;
 };
